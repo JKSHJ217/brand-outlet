23ee41c113ecafb974a9b35c06c1141a3a29484f
JKSHJ217 <garajayewyusup42@gmail.com>
2026-08-04 21:26:42 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,28 @@
+const CACHE = "brand-outlet-v1";
 
+const OFFLINE_PAGE = "/brand-outlet/calibre-run.html";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "/brand-outlet/",
+        "/brand-outlet/index.html",
+        "/brand-outlet/calibre-run.html"
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(OFFLINE_PAGE);
+    })
+  );
+});
